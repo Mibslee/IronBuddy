e692710760c93ec4d4de695e0cb029942a909059
Shane <289259059@qq.com>
2026-03-31 18:59:21 +0800

    Initial Commit

---
+++ IronBuddyTests/IronBuddyTests.swift
@@ -0,0 +1,19 @@
+//
+//  IronBuddyTests.swift
+//  IronBuddyTests
+//
+//  Created by Peishen Li on 2026/3/31.
+//
+
+import Testing
+@testable import IronBuddy
+
+struct IronBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
